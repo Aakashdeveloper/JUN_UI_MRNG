1e2b083f4215f1ee58560316f9fd8dc897d2406c
aakashDeveloper <ahanda205@gmail.com>
2019-07-08 18:20:19 +0100

    a

---
+++ javascript/first.js
@@ -0,0 +1,112 @@
+ES5=>
+
+String
+number
+boolean
+null
+undefined
+
+var a = 'javascript'
+var a = "javascript"
+*var c = 'javascript" // Not valid
+
+var b = "4646456"
+var b = 98686 //number
+var d = true //boolean
+var e = false
+var f = "true"
+
+var a = "hi123"
+
+anything between quotes is string
+numberic value is number
+true/false is boolean
+
+/******************************
+ * Operations in Javascript
+ ****************************/
++-*/%
+var a = 10
+var b = 20
+a+b
+30
+a-b
+-10
+a*b
+200
+a/b
+0.5
+
+//+ operator with string is like concat
+var a = "Hi"
+undefined
+var b = " Javascript"
+undefined
+a+b
+"Hi Javascript"
+a-b
+NaN
+a*b
+NaN
+a/b
+NaN
+
+
+var a = "hi"
+undefined
+var b = 10
+undefined
+a+b
+"hi10"
+a-b
+NaN
+
+string+string = string
+string+number = string
+number+number = number
+
+10+10+10 = 30
+10+"10"+10= "101010"
+"10"+10+10 ="101010"
+10+10+"10" ="2010"
+
+true+true+1
+"1"+true+true
+
+true
+true +true
+2
+true +true +false
+2
+false+false
+0
+"1"+true+true
+"1truetrue"
+
+true isequal to 1
+false isequal to 0
+
+true == 1
+true
+true +true
+2
+true +true +false
+2
+false+false
+0
+"1"+true+true
+"1truetrue"
+
+/**********
+ ***********/
+ console.log(a)
+VM1367:1 hi
+undefined
+alert("done")
+undefined
+confirm("do you like this")
+true
+confirm("do you like this")
+false
+prompt('What is your name')
+"jhjk"

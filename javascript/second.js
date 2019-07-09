2a987844fca7478a0cae9efb8df5a5e63c2d5bd8
aakashDeveloper <ahanda205@gmail.com>
2019-07-09 18:14:32 +0100

    a

---
+++ javascript/second.js
@@ -0,0 +1,119 @@
+modules
+
+var a = 10
+undefined
+10%2
+0
+10
+10
+10%3
+1 
+var a = 10 => assignment operator
+a == 10 is compare just value 
+a === 10 compare value with data type
+
+true == 1
+true
+true === 1
+false
+"a"==="A"
+false
+"a"==="a"
+true
+"a"=="A"
+false
+"a"=="a"
+true
+
+------------------------
+Truthy 
+0>
+true
+
+Flasey
+0 0< -1 -544 
+false
+null  => 
+undefined
+
+var a = null
+undefined
+a
+null
+a == 0
+false
+a == null
+true
+a == false
+false
+
+
+null => we can assign any value as null 
+        and can check any value exist or not 
+        null can be compared with null
+
+undefined => 
+
+
+
+var a = undefined
+undefined
+a == null
+true
+var b = undefined
+undefined
+b == null
+true
+var c
+undefined
+c == null
+true
+null == undefined
+true
+null === undefined
+false
+typeof(null)
+"object"
+typeof(undefined)
+"undefined"
+
+null is an assignment value 
+    can be assigend to variable
+
+
+undefined variable declared but not assigned to any value
+
+string
+number
+boolean
+null
+undefined
+object
+
+
+var a = undefined
+undefined
+a == null
+true
+var b = undefined
+undefined
+b == null
+true
+var c
+undefined
+c == null
+true
+null == undefined
+true
+null === undefined
+false
+typeof(null)
+"object"
+typeof(undefined)
+"undefined"
+var a = ""
+undefined
+a == null
+false
+a == undefined
+false
---
+++ javascript/third.js
@@ -0,0 +1,85 @@
+var a = 10
+var = 100
+VM1450:1 Uncaught SyntaxError: Unexpected token =
+var a= 100
+undefined
+a == 10
+false
+a == 100
+true
+var b = 20
+undefined
+a>b
+true
+a<b
+false
+a == b
+false
+a !== b
+true
+a !=== b
+VM1622:1 Uncaught SyntaxError: Unexpected token =
+a != b
+true
+a !== b
+true
+a == 100 && b == 20
+true
+a == 100 && b == 21
+false
+a == 100 || b == 21
+true
+b == 21 || a == 100
+true
+true && true
+true
+
+a == 10
+a === 10
+
+true && true
+true
+true && false
+false
+false && true
+false
+false && fasle
+false
+false && false
+false
+1&&1
+1
+1&&)
+VM1976:1 Uncaught SyntaxError: Unexpected token )
+1&&0
+0
+0&&1
+0
+0&&0
+0
+
+true && false
+false && true
+false&&false
+
+true || false
+false || true
+false || false
+
+true || false
+true
+false || true
+true
+false || false
+false
+
+!true
+
+!true
+false
+!false
+true
+var a = true
+undefined
+!a
+false

e19ad184285fba84ce675393bcb869ff6ae2aa63
aakashDeveloper <ahanda205@gmail.com>
2019-07-11 18:26:20 +0100

    a

---
+++ javascript/six.js
@@ -0,0 +1,40 @@
+function
+if else
+for
+variable
+
+var a = 10;
+if(a%2 === 0){
+    console.log('number is even')
+}else if(a%3 === 0){
+    console.log('number is dividble by 3 or 5')
+}else{
+    console.log('number is odd')
+}
+
+var a = 9;
+if(a%2 === 0){
+    console.log('number is even')
+}
+if(a%3 === 0){
+    console.log('number is dividble by 3')
+}else{
+    console.log('number is odd')
+}
+
+var a = 10;
+if(a%2 === 0){
+    console.log('number is even')
+}else if(a%3 === 0 || a%5 ===0){
+    console.log('number is dividble by 3 or 5')
+}else{
+    console.log('number is odd')
+}
+
+var output = confirm('do you want to leave');
+if(output === true){
+    console.log('okk bye')
+}else {
+    console.log('Thanks for staying')
+}
+

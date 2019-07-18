93a0856314dfb7fcea840539702ddb304d5af7c0
aakashDeveloper <ahanda205@gmail.com>
2019-07-18 19:12:18 +0100

    a

---
+++ javascript/eleve.js
@@ -0,0 +1,47 @@
+var  a =10
+console.log(a)
+
+const b = 20
+console.log(b)
+
+if(a ==10){
+    let c = "my age "
+    console.log(c+a)
+}
+
+for(i=0;i<10; i++){
+
+    let d = " ok in forlopp "
+    b = b+1
+    console.log(d + i+b)
+    console.log("inside "+b)
+
+}
+
+// console.log('outside '+c)
+
+console.log('outside '+b)
+
+
+
+a = 10
+b = a
+b
+
+var a = 13;
+var b = a;
+a = 25;
+console.log(a);
+console.log(b)
+
+/***********/
+var a;
+var b;
+a = 13
+b = a
+b = 13
+a = 25
+a
+25
+13
+
---
+++ javascript/twelve.js
@@ -0,0 +1,60 @@
+var a = [1,2,3,4,5]
+console.log(a.map(Math.sqrt))
+
+
+Map & Filter 
+In Map length of array will not Change 
+In Filter length of array can be change
+
+for => loop through a block of code a number TimeRanges
+for in loop used in case of objects
+
+var city = {"london":1378647821678, "India":872468468436868}
+var a = "";
+var b;
+for( b in city){
+    a  += city[b]
+}
+
+
+
+var color = ["red", "yellow", "green" ]
+var z;
+
+for(z of color){
+    console.log(z)
+}
+
+for(i=0;i<10;i++){
+    console.log(i)
+}
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+
+
+var i =0
+while(i<10){
+    out += i
+    i++
+}
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+
+
+console.log(i)

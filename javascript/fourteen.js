dc875e53eade6317ec9491ed099479b9d37d1475
aakashDeveloper <ahanda205@gmail.com>
2019-07-22 18:16:15 +0100

    S

---
+++ javascript/fourteen.js
@@ -0,0 +1,44 @@
+/*let numbers = [2,3,4,5,6];
+var output = numbers.map(Math.sqrt)
+var out1 = numbers.map((a) => {return a*2})
+
+console.log(out1)*/
+
+
+//let ages = [19, 16,22,43];
+
+//let checkAge = (age) => { return age > 18}
+//let out1 = ages.filter(checkAge);
+let numbers = [0,1,2,3,4];
+//let out1 = ages.filter((age) => { return age > 18});
+
+let out1 = numbers.filter((a) => {return a*2})
+console.log(out1)
+
+
+
+let numbers = [0,1,2,3,4];
+undefined
+...numbers
+numbers
+(5) [0, 1, 2, 3, 4]
+console.log(...numbers)
+VM220:1 0 1 2 3 4
+undefined
+Math.max(...numbers)
+4
+var name="javascript"
+undefined
+console.log(...name)
+VM435:1 j a v a s c r i p t
+
+
+
+{name:name}
+{name}
+
+
+[a,b] = [10,20]
+
+
+
